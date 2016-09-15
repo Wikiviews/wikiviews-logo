9aaea8e64d71bd90ca7ed172c7f1a6658827a125
DevWurm <devwurm@devwurm.net>
2016-09-15 14:57:45 +0200

    Updating build configuration

--- Makefile
+++ Makefile
@@ -1,10 +1,38 @@
-all:
+SIZE="48" "192" "500" "1000" "2000"
+
+all: pngs
 
 pngs: svg
 	mkdir -p ./exports/png
 	./render-pngs.py | /bin/sh
 
+pngs48dp: svg
+	mkdir -p ./exports/png
+	./render-pngs.py "48" | /bin/sh
+
+pngs192dp: svg
+	mkdir -p ./exports/png
+	./render-pngs.py "192" | /bin/sh
+
+pngs500dp: svg
+	mkdir -p ./exports/png
+	./render-pngs.py "500" | /bin/sh
+
+pngs1000dp: svg
+	mkdir -p ./exports/png
+	./render-pngs.py "1000" | /bin/sh
+
+pngs2000dp: svg
+	mkdir -p ./exports/png
+	./render-pngs.py "2000" | /bin/sh
+
+pngsInSize: svg
+	mkdir -p ./exports/png
+	./render-pngs.py ${SIZE} | /bin/sh
+
 svg:
 	mkdir -p ./exports/svg
 	cp ./logo.svg ./exports/svg
 
+clean:
+	rm -rf ./exports
--- render-pngs.py
+++ render-pngs.py
@@ -1,6 +1,7 @@
 #!/bin/python
 
 from itertools import product
+import sys
 
 # Outputs the imagemagick commands for rendering the logo at a specified DP size for the specified DPI screen densities
 
@@ -9,8 +10,12 @@ def measureToCommand (size, density):
 
     return "inkscape -z -e exports/png/logo-" + str(sizeInPx) + "px-" + str(size) + "dp@" + str(density) + "dpi.png -w " + str(sizeInPx) + " -h" + str(sizeInPx) + " exports/svg/logo.svg "
 
-# sizes in dp
-sizes = [48, 192, 500, 1000, 2000]
+# sizes in dp either from command line or from default setup
+sizes = []
+if len(sys.argv) > 1 :
+    sizes = map(lambda arg: int(arg), sys.argv[1:])
+else:
+    sizes = [48, 192, 500, 1000, 2000]
 
 # screen densities in DPI
 densities = [72, 96, 130, 160, 210, 240, 320, 400, 480, 640]
